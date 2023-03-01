599ca7e8a4e1caeb62ccb9c412843e06e97ced48
Austin McGee <947888+amcgee@users.noreply.github.com>
2023-03-01 16:21:19 +0100

    chore: remove scripts, scale to 3

--- cluster-ha/scripts/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "NB: Run 'seed.sh' to re-seed the database before restarting the cluster."
-echo "Stopping the cluster and remove the database storage volume..."
-docker-compose down --volumes
-echo "DONE"
